98a6e85cfa3f4ea96729b8a7e7c689dbd96a7700
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-15 14:24:40 +0300

    Implemented rook movement

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,19 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const moves: Square[] = [];
+
+        for (let i = 0; i < 8; i++) {
+            if (i !== currentSquare.col) {
+                moves.push(Square.at(currentSquare.row, i));
+            }
+
+            if (i !== currentSquare.row) {
+                moves.push(Square.at(i, currentSquare.col));
+            }
+        }
+        return moves;
     }
 }
+
