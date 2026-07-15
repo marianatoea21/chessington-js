e120ba797afa1eb885286ad98e0fa7abdbb685c2
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-15 16:20:57 +0300

    Implemented knight movement

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,24 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const moves: Square[] =[];
+
+        const directions = [
+            [2, 1], [2, -1],
+            [-2, 1], [-2, -1],
+            [1, 2], [1, -2],
+            [-1, 2], [-1, -2]
+        ]
+
+        for (const [rowOffset, colOffset] of directions) {
+            const newRow = currentSquare.row + rowOffset;
+            const newCol = currentSquare.col + colOffset;
+
+            if (newRow >= 0 && newRow <= 7 && newCol >= 0 && newCol <=7) {
+                moves.push(Square.at(newRow, newCol));
+            }
+        }
+        return moves;
     }
 }
