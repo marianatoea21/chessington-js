6d7f382f26f6c0ab04ef2b3506e3e23153968815
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-15 14:43:09 +0300

    Implemented bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,30 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const moves: Square[] = [];
+
+        const directions = [
+            [1, 1],
+            [1, -1],
+            [-1, 1],
+            [-1, -1]
+        ]
+
+        for (const [rowOffset, colOffset] of directions) {
+            let i = 1;
+            while (true) {
+                const newRow = currentSquare.row + rowOffset * i;
+                const newCol = currentSquare.col + colOffset * i;
+
+                if (newRow < 0 || newRow > 7 || newCol > 7 || newCol < 0) {
+                    break;
+                }
+
+                moves.push(Square.at(newRow, newCol));
+                i++;
+            }
+        }
+        return moves;
     }
 }
