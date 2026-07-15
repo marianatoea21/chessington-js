66ae7d98a14d21c85af80cbc93187f7b4f1e3ce3
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-15 12:45:30 +0300

    git commit -am "White and black pawns can move one square forward."

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,13 +1,25 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        const currentSquare = board.findPiece(this);
+        const moves: Square[] = [];
+
+        if (this.player === Player.WHITE) {
+            moves.push(Square.at(currentSquare.row + 1, currentSquare.col));
+        }
+
+        if (this.player === Player.BLACK) {
+            moves.push(Square.at(currentSquare.row - 1, currentSquare.col));
+        }
+
+        return moves;
     }
 }
