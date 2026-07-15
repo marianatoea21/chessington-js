70b210ef7c62a09a6badc1608a2abc307480e733
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-15 16:34:31 +0300

    Implemented king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,23 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const moves: Square[] = [];
+
+        const directions = [
+            [1, 1], [1, -1], [-1, 1], [-1, -1],
+            [1, 0], [0, 1], [-1, 0], [0, -1]
+        ]
+
+        for (const [rowOffset, colOffset] of directions) {
+            const newRow = currentSquare.row + rowOffset;
+            const newCol = currentSquare.col + colOffset;
+
+            if (newRow >= 0 && newRow <= 7 && newCol >= 0 && newCol <=7) {
+                moves.push(Square.at(newRow, newCol));
+            }
+        }
+
+        return moves;
     }
 }
