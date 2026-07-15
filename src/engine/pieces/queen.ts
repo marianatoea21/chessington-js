a3eb914af7c8191965e321269671320c3d96e67e
Mariana Toea <Mariana.Toea@softwire.com>
2026-07-15 15:22:10 +0300

    Implemented queen movement

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,41 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare: Square = board.findPiece(this);
+        const moves: Square[] = [];
+
+        const directions = [
+            [1, 1],
+            [1, -1],
+            [-1, 1],
+            [-1, -1]
+        ]
+
+        for (const [rowOffset, colOffset] of directions) {
+            let i = 1;
+            while (true) {
+                const newRow = currentSquare.row + rowOffset * i;
+                const newCol = currentSquare.col + colOffset * i;
+
+                if (newRow < 0 || newRow > 7 || newCol > 7 || newCol < 0) {
+                    break;
+                }
+
+                moves.push(Square.at(newRow, newCol));
+                i++;
+            }
+        }
+
+        for (let i = 0; i < 8; i++) {
+            if (i !== currentSquare.col) {
+                moves.push(Square.at(currentSquare.row, i));
+            }
+
+            if (i !== currentSquare.row) {
+                moves.push(Square.at(i, currentSquare.col));
+            }
+        }
+
+        return moves;
     }
 }
